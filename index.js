577bf98aa1771cd0697fab8e4b6e7606066cd8f6
Alexander Balyshyn <balyszyn@gmail.com>
2019-01-09 17:13:01 +0200

    add skeleton of page and p5.js canvas intialization

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-
+let canvasWindowSpacingPercent = 10
 function setup () {
-  createCanvas(640, 360)
+  let createdCanvas = createCanvas(windowWidth, calcWithWindowSpacing(windowHeight))
+  createdCanvas.parent('canvas-container')
+}
+/**
+ * Overriding of p5 listeners for resizing of window.
+ * Resize {@code createdCanvas} according to windows height and width.
+ */
+function windowResized () {
+  console.debug(windowWidth + " and " + windowHeight)
+  resizeCanvas(windowWidth, calcWithWindowSpacing(windowHeight))
 }
 
 function draw () {
-  background(51)
-  for (let boid of flock) {
-    boid.edges()
-    boid.flock(flock)
-    boid.update()
-    boid.show()
-  }
+  background(23)
+}
+
+const calcWithWindowSpacing = (value) => {
+  return value - value * canvasWindowSpacingPercent / 100 
 }
